d525a5fddc4df582f79e970036fe591b8c4d69ed
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 05:24:00 +0700

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -33,86 +33,43 @@ sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 
-OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{print $1}'`
-OhpDB=`cat /root/log-install.txt | grep -w "OHP DBear" | cut -d: -f2 | awk '{print $1}'`
-OhpOVPN=`cat /root/log-install.txt | grep -w "OHP OpenVPN" | cut -d: -f2 | awk '{print $1}'`
-
 sleep 1
 clear
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
 
-if [[ ! -z "${PID}" ]]; then
-# // resttart slowdns
-pkill server
-pkill client
-systemctl daemon-reload
-systemctl stop client
-systemctl stop server
-systemctl enable client
-systemctl enable server
-systemctl start client
-systemctl start server
-systemctl restart client
-systemctl restart server
-clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[0;41;36m    SSH Account      \E[0m" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Username : $Login" | tee -a /etc/log-create-user.log
-echo -e "Password : $Pass" | tee -a /etc/log-create-user.log
-echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "IP : $IP" | tee -a /etc/log-create-user.log
-echo -e "Host : $domen" | tee -a /etc/log-create-user.log
-echo -e "OpenSSH : $opensh" | tee -a /etc/log-create-user.log
-echo -e "Dropbear : $db" | tee -a /etc/log-create-user.log
-echo -e "SSH-WS : $portsshws" | tee -a /etc/log-create-user.log
-echo -e "SSH-SSL-WS : $wsssl" | tee -a /etc/log-create-user.log
-echo -e "SSL/TLS : $ssl" | tee -a /etc/log-create-user.log
-#echo -e "Port Squid : $sqd" | tee -a /etc/log-create-user.log
-echo -e "UDPGW : 7100-7300" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-#echo -e "OpenVPN Config : http://$IP:81/" | tee -a /etc/log-create-user.log
-#echo -e "\033[0;34m━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Payload WS" | tee -a /etc/log-create-user.log
-echo -e "
-GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]
-" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-
-else
-
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[0;41;36m SSH Account    \E[0m" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Username        : $Login" | tee -a /etc/log-create-user.log
-echo -e "Password        : $Pass" | tee -a /etc/log-create-user.log
-echo -e " Host Slowdns : ${NS}" | tee -a /etc/log-create-user.log
-echo -e " Pub Key      : ${PUB}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m"
-echo -e "IP              : $IP" | tee -a /etc/log-create-user.log
-echo -e "Host            : $domen" | tee -a /etc/log-create-user.log
-echo -e "OpenSSH         : $opensh" | tee -a /etc/log-create-user.log
-echo -e "Dropbear        : $db" | tee -a /etc/log-create-user.log
-echo -e "SSH-WS          : $portsshws" | tee -a /etc/log-create-user.log
-echo -e "SSH-SSL-WS      : $wsssl" | tee -a /etc/log-create-user.log
-echo -e "SSL/TLS         :$ssl" | tee -a /etc/log-create-user.log
-echo -e "SSH-WS OVPN     : 2086"
-echo -e "Port Squid      :$sqd" | tee -a /etc/log-create-user.log
-echo -e "UDPGW           : 7100-7300" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "OpenVPN Config : http://$IP:81/" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Expired On      : $exp" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "
-GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]
-" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-fi
-echo "" | tee -a /etc/log-create-user.log
+echo -e " ━━━━━━━━━━━━━━━" | lolcat
+echo -e " SSH Account    " | lolcat
+echo -e " ━━━━━━━━━━━━━━━" | lolcat
+echo -e " Username        : $Login" | lolcat
+echo -e " Password        : $Pass" | lolcat
+echo -e " Host Slowdns    : ${NS}" | lolcat
+echo -e " Pub Key         : ${PUB}" | lolcat
+echo -e " ━━━━━━━━━━━━━━━"
+echo -e " IP              : $IP" | lolcat
+echo -e " Host            : $domen" | lolcat
+echo -e " OpenSSH         : $opensh" | lolcat
+echo -e " Dropbear        : $db" | lolcat
+echo -e " SSH-WS          : $portsshws" | lolcat
+echo -e " SSH-SSL-WS      : $wsssl" | lolcat
+echo -e " SSL/TLS         : $ssl" | lolcat
+echo -e " SSH-WS OVPN     : 2086" | lolcat
+echo -e " Port Squid      : $sqd" | lolcat
+echo -e " UDPGW           : 7100-7300" | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " OpenVPN Config  : http://$domen:81/" | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " Expired On      : $exp" | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " Payload Websocket HTTP : " | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " GET / HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]" | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " Payload Websocket SSL/TLS : " | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " GET wss://bug.com/ HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]" | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e "" | lolcat
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
