47642ea4db5fe8bc4ba644558631ba19164bb8ec
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-22 16:20:06 +0700

    Create cf.sh

---
+++ install/cf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ==========================================
+# Getting
+
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+
+clear
+
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c2)
+DOMAIN=munsc.me
+SUB_DOMAIN=${sub}.munsc.me
+CF_ID=mbahkoclok22@gmail.com
+CF_KEY=bacbaf2dd9f9808ce5b1fcf562d137175e7d2
+
+set -euo pipefail
+
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${MYIP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${MYIP}'","ttl":120,"proxied":false}')
+
+echo ""
+
+echo "Host : $SUB_DOMAIN"
+sleep 5
+echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh
