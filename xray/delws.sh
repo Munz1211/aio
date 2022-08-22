e288e1394e2bd5fd700b627304692b0241a2f2ff
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 06:45:46 +0700

    Update delws.sh

--- xray/delws.sh
+++ xray/delws.sh
@@ -12,28 +12,29 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\\E[0;41;36m       Delete Vmess Account        \E[0m"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+                echo -e "       Delete Vmess Account        " | lolcat
+                echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 		echo ""
-		echo "You have no existing clients!"
+		echo -e "   You have no existing clients!   " | lolcat
 		echo ""
-		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 		read -n 1 -s -r -p "Press any key to back on menu"
-    menu
+		menu
 	fi
 
 	clear
-	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  echo -e "\\E[0;41;36m       Delete Vmess Account        \E[0m"
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  echo "  User       Expired  " 
-	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
-    red "tap enter to go back"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	  read -rp "Input Username : " user
+		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+                echo -e "       Delete Vmess Account        " | lolcat
+                echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+		echo -e ""
+		echo -e "  	User       	Expired     " | lolcat
+		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+		grep -e "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+    		echo ""
+    		echo -e "tap enter to go back" | lolcat
+    		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+	  	read -rp "Input Username : " user
     if [ -z $user ]; then
     menu
     else
@@ -41,12 +42,12 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " V2RAY Account Deleted Successfully"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " Client Name : $user"
-    echo " Expired On  : $exp"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+    echo -e "  V2RAY Account Deleted Successfully" | lolcat
+    echo -e " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+    echo -e "  Client Name : $user" | lolcat
+    echo -e "  Expired On  : $exp" | lolcat
+    echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
     
