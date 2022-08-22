448b28715e6ebc2ab39b8862a76efaa64fc5fc44
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-22 18:22:52 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -32,13 +32,6 @@ export BOLD="\e[1m"
 export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
-# // Exporting URL Host
-export Server_URL="autosc.me/aio"
-export Server_Port="443"
-export Server_IP="underfined"
-export Script_Mode="Stable"
-export Auther="FsidVPN"
-
 # // Root Checking
 if [ "${EUID}" -ne 0 ]; then
 		echo -e "${EROR} Please Run This Script As Root User !"
