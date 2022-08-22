937fd02ec585a2614211c0486f9cca41b871d3ce
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 05:43:37 +0700

    Update member.sh

--- ssh/member.sh
+++ ssh/member.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-#########################
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
+echo "Checking VPS"
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;41;36m                 MEMBER SSH               \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"      
-echo "USERNAME          EXP DATE          STATUS"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "---------------------------------------------------" | lolcat
+echo "USERNAME          EXP DATE          STATUS" | lolcat
+echo "---------------------------------------------------" | lolcat
 while read expired
 do
 AKUN="$(echo $expired | cut -d: -f1)"
@@ -23,16 +12,16 @@ exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
 status="$(passwd -S $AKUN | awk '{print $2}' )"
 if [[ $ID -ge 1000 ]]; then
 if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}"
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}" | lolcat
 else
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}"
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}" | lolcat
 fi
 fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "Account number: $JUMLAH user"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -n 1 -s -r -p "Press any key to back on menu"
-
+echo "---------------------------------------------------" | lolcat
+echo "Account number: $JUMLAH user" | lolcat
+echo "---------------------------------------------------" | lolcat
+echo -e ""
+read -p "$( echo -e "Press [ Enter ] To Menu")"
 menu
