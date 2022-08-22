600c6cc451853b3e51178068438baacd5132597e
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 06:35:20 +0700

    Update addvless.sh

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -3,13 +3,13 @@ domain=$(cat /root/domain)
 
 clear
 
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m Add Xray/Vless Account \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " ━━━━━━━━━━━━━━━━━ " | lolcat
+echo -e " Add Vless Account " | lolcat
+echo -e " ━━━━━━━━━━━━━━━━━ " | lolcat
 read -rp "User: " -e user
 egrep -w "^### $user" /etc/xray/config.json >/dev/null
 if [ $? -eq 0 ]; then
-echo -e "Username Sudah Ada"
+echo -e "Username Sudah Ada" | lolcat
 exit 0
 fi
 uuid=$(cat /proc/sys/kernel/random/uuid)
@@ -25,25 +25,25 @@ vlesslink3="vless://${uuid}@${domain}:443?mode=gun&security=tls&encryption=none&
 systemctl restart xray
 clear
 echo -e ""
-echo -e "━━━-XRAY/VLESS-━━━━"
-echo -e "Remarks        : ${user}"
-echo -e "Domain         : ${domain}"
-echo -e "port TLS       : 443"
-echo -e "port GRPC      : 443"
-echo -e "port none TLS  : 80"
-echo -e "id             : ${uuid}"
-echo -e "Encryption     : none"
-echo -e "network        : ws"
-echo -e "path           : /vless"
-echo -e "path           : /vless-grpc"
-echo -e "━━━━━━━━━━━━━━━"
-echo -e "link TLS       : ${vlesslink1}"
-echo -e "━━━━━━━━━━━━━━━"
-echo -e "link none TLS  : ${vlesslink2}"
-echo -e "━━━━━━━━━━━━━━━"
-echo -e "link GRPC      : ${vlesslink3}"
-echo -e "━━━━━━━━━━━━━━━"
-echo -e "Expired On     : $exp"
+echo -e "━━━-XRAY/VLESS-━━━━" | lolcat
+echo -e "Remarks        : ${user}" | lolcat
+echo -e "Domain         : ${domain}" | lolcat
+echo -e "port TLS       : 443" | lolcat
+echo -e "port GRPC      : 443" | lolcat
+echo -e "port none TLS  : 80" | lolcat
+echo -e "id             : ${uuid}" | lolcat
+echo -e "Encryption     : none" | lolcat
+echo -e "network        : ws" | lolcat
+echo -e "path           : /vless" | lolcat
+echo -e "path           : /vless-grpc" | lolcat
+echo -e "━━━━━━━━━━━━━━━" | lolcat
+echo -e "link TLS       : ${vlesslink1}" | lolcat
+echo -e "━━━━━━━━━━━━━━━" | lolcat
+echo -e "link none TLS  : ${vlesslink2}" | lolcat
+echo -e "━━━━━━━━━━━━━━━" | lolcat
+echo -e "link GRPC      : ${vlesslink3}" | lolcat
+echo -e "━━━━━━━━━━━━━━━" | lolcat
+echo -e "Expired On     : $exp" | lolcat
 echo -e ""
 echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
