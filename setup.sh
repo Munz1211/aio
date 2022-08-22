a96e2a30ec433e7d1afd50edd0dadc65e433579d
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-22 18:28:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 #======================================
+
+#INTALL LOLCAT
+wget https://raw.githubusercontent.com/Munz1211/aio/main/file/lolcat.sh && chmod +x lolcat.sh && screen -S lolcat ./lolcat.sh
+
 cd /root
 
 #System version number
