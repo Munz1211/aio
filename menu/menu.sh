d9a6f84d82ef5d03e0496e431a879e15820f5c38
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 06:21:11 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -3,6 +3,17 @@
 # Edition : Stable Edition V1.0
 # =========================================
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+NC='\033[0m'
+
+IP=$( curl -s https://ipinfo.io/ip/ )
+
 freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
 	tram=$( free -m | awk 'NR==2 {print $2}' )
 	swap=$( free -m | awk 'NR==4 {print $2}' )
