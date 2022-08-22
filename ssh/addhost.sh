cc60de9a828f6edfb33f482c3a4e1fb547797688
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-22 18:02:30 +0700

    Create addhost.sh

---
+++ ssh/addhost.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#########################
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+clear
+#echo -e "\033[0;34m━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -rp "Domain/Host: " -e host
+echo ""
+if [ -z $host ]; then
+echo "????"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━\033[0m"
+read -n 1 -s -r -p "Press any key to back on menu"
+setting-menu
+else
+echo "$host" > /root/domain
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━\033[0m"
+echo "Dont forget to renew cert"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
