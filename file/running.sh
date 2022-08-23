bd6aa791786e4b90b751bc118303c95391751b91
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-23 07:54:31 +0700

    Update running.sh

--- file/running.sh
+++ file/running.sh
@@ -4,9 +4,6 @@
 # =========================================
 
 # // Export Color & Information
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 NC='\033[0m'
 
 freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
@@ -26,57 +23,57 @@ OS_Kernel="$( uname -r )"
 # // OpenSSH
 openssh=$( systemctl status ssh | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $openssh == "running" ]]; then
-    status_openssh="${GREEN}Running${NC} ( No Eror )"
+    status_openssh="Running${NC} ( No Error )"
 else
-    status_openssh="${RED}No Running${NC} ( Eror )"
+    status_openssh="No Running${NC} ( Error )"
 fi
 
 # // Stunnel5
 stunnel5=$( systemctl status stunnel4 | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $stunnel5 == "running" ]]; then
-    status_stunnel5="${GREEN}Running${NC} ( No Eror )"
+    status_stunnel5="Running${NC} ( No Error )"
 else
-    status_stunnel5="${RED}No Running${NC} ( Eror )"
+    status_stunnel5="No Running${NC} ( Error )"
 fi
 
 # // Dropbear
 dropbear=$( systemctl status dropbear | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $dropbear == "running" ]]; then
-    status_dropbear="${GREEN}Running${NC} ( No Eror )"
+    status_dropbear="Running${NC} ( No Error )"
 else
-    status_dropbear="${RED}No Running${NC} ( Eror )"
+    status_dropbear="No Running${NC} ( Error )"
 fi
 
 # // Squid
 squid=$( systemctl status squid | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $squid == "running" ]]; then
-    status_squid="${GREEN}Running${NC} ( No Eror )"
+    status_squid="Running${NC} ( No Error )"
 else
-    status_squid="${RED}No Running${NC} ( Eror )"
+    status_squid="No Running${NC} ( Error )"
 fi
 
 # // SSH Websocket Proxy
 ssh_ws=$( systemctl status ws-nontls | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $ssh_ws == "running" ]]; then
-    status_ws_epro="${GREEN}Running${NC} ( No Eror )"
+    status_ws_epro="Running${NC} ( No Error )"
 else
-    status_ws_epro="${RED}No Running${NC} ( Eror )"
+    status_ws_epro="No Running${NC} ( Error )"
 fi
 
 # // Trojan Proxy
 ss=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $ss == "running" ]]; then
-    status_ss="${GREEN}Running${NC} ( No Eror )"
+    status_ss="Running${NC} ( No Error )"
 else
-    status_ss="${RED}No Running${NC} ( Eror )"
+    status_ss="No Running${NC} ( Error )"
 fi
 
 # // NGINX
 nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $nginx == "running" ]]; then
-    status_nginx="${GREEN}Running${NC} ( No Eror )"
+    status_nginx="Running${NC} ( No Error )"
 else
-    status_nginx="${RED}No Running${NC} ( Eror )"
+    status_nginx="No Running${NC} ( Error )"
 fi
 
 # =========================================================================================================
@@ -106,34 +103,31 @@ clear
 clear && clear && clear
 clear;clear;clear
 
-echo -e "${YELLOW}----------------------------------------------------------${NC}"
-echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "${YELLOW}----------------------------------------------------------${NC}"
+echo -e "----------------------------------------------------------" | lolcat
+echo -e "                ( STATUS SERVICE INFORMATION )            " | lolcat
+echo -e "----------------------------------------------------------" | lolcat
 echo ""
-echo -e "${RED_BG}                     Sytem Information                    ${NC}"
-echo -e "Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) "
-echo -e "Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
-echo -e "Script Version      = 3.1"
-echo -e "Operating System    = ${OS_FName} ( ${OS_Arch} )"
-echo -e "Total Amount RAM    = $tram MB"
-echo -e "Current Domain      = $(cat /root/domain)"
-echo -e "Server IP           = ${IP}"
+echo -e "                     Sytem Information                    " | lolcat
+echo -e "Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) " | lolcat
+echo -e "Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )" | lolcat
+echo -e "Script Version      = 3.1" | lolcat
+echo -e "Operating System    = ${OS_FName} ( ${OS_Arch} )" | lolcat
+echo -e "Total Amount RAM    = $tram MB" | lolcat
+echo -e "Current Domain      = $(cat /root/domain)" | lolcat
+echo -e "Server IP           = ${IP}" | lolcat
 echo ""
-echo -e "${RED_BG}                     Service Information                  ${NC}"
+echo -e "                     Service Information                  " | lolcat
 echo -e ""
-echo -e "OpenSSH             = $status_openssh"
-echo -e "Dropbear            = $status_dropbear"
-echo -e "Stunnel4            = $status_stunnel5"
-echo -e "Squid               = $status_squid"
-echo -e "NGINX               = $status_nginx"
-echo -e "SSH NonTLS          = $status_ws_epro"
-echo -e "SSH TLS             = $status_ws_epro"
-echo -e "Vmess WS/GRPC       = $status_ss"
-echo -e "Vless WS/GRPC       = $status_ss"
-echo -e "Trojan WS/GRPC      = $status_ss"
-echo ""
-echo -e "${GREEN}Please Type Menu${NC}"
-echo -e ""
+echo -e "OpenSSH             = $status_openssh" | lolcat
+echo -e "Dropbear            = $status_dropbear" | lolcat
+echo -e "Stunnel4            = $status_stunnel5" | lolcat
+echo -e "Squid               = $status_squid" | lolcat
+echo -e "NGINX               = $status_nginx" | lolcat
+echo -e "SSH NonTLS          = $status_ws_epro" | lolcat
+echo -e "SSH TLS             = $status_ws_epro" | lolcat
+echo -e "Vmess WS/GRPC       = $status_ss" | lolcat
+echo -e "Vless WS/GRPC       = $status_ss" | lolcat
+echo -e "Trojan WS/GRPC      = $status_ss" | lolcat
 echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
