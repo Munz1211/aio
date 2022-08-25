b72be562a42a302b45557e84284fcd26d403f5a8
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-26 06:35:48 +0700

    Update menu1.sh

--- menu/menu1.sh
+++ menu/menu1.sh
@@ -6,11 +6,10 @@ echo -e "  \e[1;32m------------------------------------------------------------\
 echo -e "                             SYSTEM MENU\e[m" | lolcat 
 echo -e "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e "   1    Add Host Sub Domain" | lolcat
-echo -e "   2    Activated SSH WS" | lolcat
-echo -e "   3    Check Usage User" | lolcat
-echo -e "   4    Reboot VPS" | lolcat
-echo -e "   5    Speedtest VPS" | lolcat
-echo -e "   6    Running All Service" | lolcat
+echo -e "   2    Check Usage User" | lolcat
+echo -e "   3    Reboot VPS" | lolcat
+echo -e "   4    Speedtest VPS" | lolcat
+echo -e "   5    Running All Service" | lolcat
 echo -e ""
 echo -e "   x    Back To Menu" | lolcat
 echo -e ""
@@ -21,18 +20,15 @@ case $menu in
 addhost
 ;;
 2)
-sshws
-;;
-3)
 cekusage
 ;;
-4)
+3)
 reboot
 ;;
-5)
+4)
 speedtest
 ;;
-6)
+5)
 running
 ;;
 x)
