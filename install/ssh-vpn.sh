e3ffd4b8282fd26f8dea6233fa477d06f1415a10
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-26 02:32:27 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -137,12 +137,20 @@ install_ssl(){
 
 
 # install webserver
-apt -y install nginx
-cd
+echo -e "[ ${green}INFO${NC} ] install webserver"
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl >/dev/null 2>&1
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Munz1211/aio/main/ssh/nginx.conf"
+curl https://raw.githubusercontent.com/Munz1211/aio/main/ssh/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/Munz1211/aio/main/ssh/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
 mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/Munz1211/aio/main/ssh/index.html1"
 /etc/init.d/nginx restart
 /etc/init.d/nginx status
 
@@ -267,7 +275,7 @@ chmod +x install.sh
 
 # banner /etc/issue.net
 echo -e "[ ${green}INFO${NC} ] Install Banner"
-wget -O /etc/issue.net "https://raw.githubusercontent.com/Munz1211/scmunz/main/ssh/banner.conf"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/Munz1211/aio/main/ssh/banner.conf"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
