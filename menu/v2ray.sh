7ad82a1d0547b8913328bcf160694bd6e14918d8
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-26 03:35:11 +0700

    Create v2ray.sh

---
+++ menu/v2ray.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Checking VPS"
+sleep 2
+
+clear
+echo -e ""
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "             All Account V2Ray  " | lolcat
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e ""
+echo -e " 1 Add Vmess Account" | lolcat
+echo -e " 2 Add Vless Account" | lolcat
+echo -e " 3 Add Trojan Account" | lolcat
+echo -e " 4 Cek User Xray" | lolcat
+echo -e " 5 Del User Xray" | lolcat
+echo -e " 6 Renew User Xray" | lolcat
+echo -e ""
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e " x)   MENU" | lolcat
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e ""
+read -p "     Please Input Number  [1-4 or x] :  "  v2ray
+echo -e ""
+case $v2ray in
+1)
+addws
+;;
+2)
+addvless
+;;
+3)
+addtr
+;;
+4)
+cekws
+;;
+5)
+delws
+;;
+6)
+renewws
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+;;
+esac
