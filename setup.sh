665e1c2c3d0990e0ed9c6624c246e589606eaef0
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-25 17:12:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,11 @@
 MYIP=$(curl -sS ipv4.icanhazip.com)
 #============================
 clear
-red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
-tyblue='\e[1;36m'
 NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
 #======================================
 
 cd /root
