7e2d6e34ee353e204be6c5142f619af0ef63b448
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-26 06:18:26 +0700

    Update sshws.sh

--- ssh/sshws.sh
+++ ssh/sshws.sh
@@ -27,6 +27,7 @@ if [ -f "/etc/systemd/system/sshws.service" ]; then
 clear
 else
 wget -q -O /usr/bin/proxy3.js "https://raw.githubusercontent.com/Munz1211/aio/main/tambahan/proxy3.js"
+
 cat > /etc/systemd/system/sshws.service <<-END
 [Unit]
 Description=WSenabler
@@ -45,7 +46,11 @@ END
 
 fi
 
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Green_font_prefix="\033[32m"  
+Red_font_prefix="\033[31m"
+Green_background_prefix="\033[42;37m"
+Red_background_prefix="\033[41;37m"
+Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
 
