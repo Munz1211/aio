37d2030809cdf7388a1a46e2ac84153c5f90aa06
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-25 17:17:34 +0700

    Update tools.sh

--- file/tools.sh
+++ file/tools.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 clear
-red='\e[1;31m'
-green='\e[1;32m'
-yell='\e[1;33m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 if [[ -e /etc/debian_version ]]; then
 	source /etc/os-release
@@ -61,6 +55,7 @@ fi
 sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+clear
 yellow "Dependencies successfully installed..."
-sleep 5
+sleep 10
 clear
