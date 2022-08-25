5a66d299739c0d2174d8277affdb4a8d0b959049
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-26 03:20:22 +0700

    Create ssh.sh

---
+++ menu/ssh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Checking VPS"
+sleep 5
+
+clear
+echo -e ""
+echo -e "═════════════════════════════════════════" | lolcat
+echo -e "             SSH & OpenVPN Menu  " | lolcat
+echo -e "═════════════════════════════════════════" | lolcat
+echo -e " 1   Add User SSH Account" | lolcat
+echo -e " 2   Cek User SSH Account" | lolcat
+echo -e " 3   Del User SSH Account" | lolcat
+echo -e " 4   Renew User SSH Account" | lolcat
+echo -e " 5   Member User SSH Account" | lolcat
+echo -e ""
+echo -e "═════════════════════════════════════════" | lolcat
+echo -e " x)   MENU" | lolcat
+echo -e "═════════════════════════════════════════" | lolcat
+echo -e ""
+read -p "     Please Input Number  [ 1 - 5 or x ] :  "  ssh
+echo -e ""
+case $ssh in
+1)
+addssh
+;;
+2)
+cek
+;;
+3)
+hapus
+;;
+4)
+renew
+;;
+5)
+member
+;;
+x)
+exit
+;;
+*)
+echo "Please enter an correct number"
+exit
+;;
+esac
