d3ccf04418d7a968ad8b25f5a4a081dff5cd5d3c
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-25 15:47:17 +0700

    Update slowdns.sh

--- install/slowdns.sh
+++ install/slowdns.sh
@@ -1,36 +1,10 @@
-#!/bin/bash
-# Menu Setup
-# Edition : Stable Edition V1.0
-# =========================================
+#!/bin/bas
 
-# // Exporting Language to UTF-8
-export LC_ALL='en_US.UTF-8' >/dev/null 2>&1
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US.UTF-8'
-export LC_CTYPE='en_US.utf8'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
-
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
-
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
 clear
 # ===============================================
 cd
