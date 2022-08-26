02a43cc3c5140ad9ab6e755da65848ea3d2d6976
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-27 01:57:05 +0700

    Update menu1.sh

--- menu/menu1.sh
+++ menu/menu1.sh
@@ -5,30 +5,27 @@ clear
 echo -e "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e "                             SYSTEM MENU\e[m" | lolcat 
 echo -e "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
-echo -e "   1    Add Host Sub Domain" | lolcat
-echo -e "   2    Check Usage User" | lolcat
-echo -e "   3    Reboot VPS" | lolcat
-echo -e "   4    Speedtest VPS" | lolcat
-echo -e "   5    Running All Service" | lolcat
+echo -e ""
+echo -e "   1    Check Usage User" | lolcat
+echo -e "   2    Reboot VPS" | lolcat
+echo -e "   3    Speedtest VPS" | lolcat
+echo -e "   4    Running All Service" | lolcat
 echo -e ""
 echo -e "   x    Back To Menu" | lolcat
 echo -e ""
-read -p "   Select From Options [ 1 - 6 or x ] :  " menu
+read -p "   Select From Options [ 1 - 4 or x ] :  " menu
 echo -e ""
 case $menu in
 1)
-addhost
-;;
-2)
 cekusage
 ;;
-3)
+2)
 reboot
 ;;
-4)
+3)
 speedtest
 ;;
-5)
+4)
 running
 ;;
 x)
