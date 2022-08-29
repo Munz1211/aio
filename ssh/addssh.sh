5a51a3a0e4be35d729388c78a19b48d91a315bc4
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-30 06:45:23 +0700

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -3,17 +3,16 @@
 MYIP=$(curl -sS ipv4.icanhazip.com)
 
 clear
-
-domen=`cat /root/domain`
-
-wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | awk '{print $1}'`
+echo "Checking VPS"
+sleep 2
 
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
+domen=`cat /root/domain`
+wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | awk '{print $1}'`
 IP=$(curl -sS ifconfig.me);
-
 ossl=`cat /root/log-install.txt | grep -w "OpenVPN" | cut -f2 -d: | awk '{print $6}'`
 opensh=`cat /root/log-install.txt | grep -w "OpenSSH" | cut -f2 -d: | awk '{print $1}'`
 db=`cat /root/log-install.txt | grep -w "Dropbear" | cut -f2 -d: | awk '{print $1,$2}'`
@@ -21,11 +20,22 @@ ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 
 sleep 1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+
 clear
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e " ━━━━━━━━━━━━━━━" | lolcat
 echo -e " SSH Account    " | lolcat
 echo -e " ━━━━━━━━━━━━━━━" | lolcat
@@ -55,6 +65,6 @@ echo -e " Payload Websocket SSL/TLS : " | lolcat
 echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e " GET wss://bug.com/ HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]" | lolcat
 echo -e " ━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "" | lolcat
-read -n 1 -s -r -p "Press any key to back on menu"
+echo -e ""
+read -p "$( echo -e "Press [ Enter ] To Menu")"
 menu
