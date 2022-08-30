e6856dbef2d212291690dcf21341b91c15317b95
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-30 07:09:25 +0700

    Update hapus.sh

--- ssh/hapus.sh
+++ ssh/hapus.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
+
 clear
+echo "Checking VPS"
+sleep 2
+
+clear
+
 echo "---------------------------------------------------" | lolcat
 echo "USERNAME          EXP DATE          STATUS" | lolcat
 echo "---------------------------------------------------" | lolcat
 while read expired
 do
+
 AKUN="$(echo $expired | cut -d: -f1)"
 ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
 exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
 status="$(passwd -S $AKUN | awk '{print $2}' )"
+
 if [[ $ID -ge 1000 ]]; then
 if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}" | lolcat
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED" | lolcat
 else
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}" | lolcat
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED" | lolcat
 fi
 fi
 done < /etc/passwd
