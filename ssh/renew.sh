5fa701b3a7745c2ea7cf6fe7772e8cf2e3f3b77d
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-08-30 07:11:10 +0700

    Update renew.sh

--- ssh/renew.sh
+++ ssh/renew.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
+
 clear
+echo "Checking VPS"
+sleep 2
+
+clear
+
 read -p "         Username       :  " User
 egrep "^$User" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 read -p "         Day Extend     :  " Days
+
 Today=`date +%s`
 Days_Detailed=$(( $Days * 86400 ))
 Expire_On=$(($Today + $Days_Detailed))
@@ -16,6 +18,7 @@ Expiration=$(date -u --date="1970-01-01 $Expire_On sec GMT" +%Y/%m/%d)
 Expiration_Display=$(date -u --date="1970-01-01 $Expire_On sec GMT" '+%d %b %Y')
 passwd -u $User
 usermod -e  $Expiration $User
+
 egrep "^$User" /etc/passwd >/dev/null
 echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
 clear
