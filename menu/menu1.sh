dfa1d5a8cef9198a273086f8c15e64ef7da04e0d
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-09-19 18:54:56 +0700

    Update menu1.sh

--- menu/menu1.sh
+++ menu/menu1.sh
@@ -13,6 +13,8 @@ echo -e "   1    Check Usage User" | lolcat
 echo -e "   2    Reboot VPS" | lolcat
 echo -e "   3    Speedtest VPS" | lolcat
 echo -e "   4    Running All Service" | lolcat
+echo -e "   5    Add Host Domain" | lolcat
+echo -e "   6    Restart Service" | lolcat
 echo -e ""
 echo -e "   x    Back To Menu" | lolcat
 echo -e ""
@@ -30,6 +32,12 @@ speedtest
 ;;
 4)
 running
+;;
+5)
+addhost
+;;
+6)
+xp
 ;;
 x)
 exit
